fe1083fd256f399e366df6aa5d806620a7abcfc6
danielglez4234 <56640190+danielglez4234@users.noreply.github.com>
2021-07-20 14:32:06 +0100

    test para recibir datos en Pug
    configuración de router,.get en js, y variables en Pug

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const router = express.Router();
-
-
+const project = require('../data.json').projects;
+const camaras = require('../models/camaras');
 
 // rendering index.pug with the data
-router.get('/', async (req, res) => {
-  // const indexTemplateData = { project }
-  res.render('index');
+router.get('/', (req, res) => {
+  const indexTemplateData = { project }
+  res.render('index', indexTemplateData);
 });
 
 module.exports = router;
